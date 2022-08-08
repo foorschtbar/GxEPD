d6394edb47e94243c0e5b6416dcad51e36a0400e
foorschtbar <mail@wischmopp90.com>
2022-08-08 20:29:21 +0200

    Fixed HINKE0154A35 and HINKE042A11

--- src/HINKE0154A35/HINKE0154A35.cpp
+++ src/HINKE0154A35/HINKE0154A35.cpp
@@ -21,62 +21,68 @@
 // Partial Update Delay, may have an influence on degradation
 #define HINKE0154A35_PU_DELAY 500
 
-HINKE0154A35::HINKE0154A35(GxIO& io, int8_t rst, int8_t busy)
-  : GxEPD(HINKE0154A35_WIDTH, HINKE0154A35_HEIGHT), IO(io),
-    _current_page(-1), _using_partial_mode(false), _diag_enabled(false),
-    _rst(rst), _busy(busy)
+HINKE0154A35::HINKE0154A35(GxIO &io, int8_t rst, int8_t busy)
+    : GxEPD(HINKE0154A35_WIDTH, HINKE0154A35_HEIGHT), IO(io),
+      _current_page(-1), _using_partial_mode(false), _diag_enabled(false),
+      _rst(rst), _busy(busy)
 {
 }
 
 void HINKE0154A35::drawPixel(int16_t x, int16_t y, uint16_t color)
 {
   swap(x, y);
-  if ((x < 0) || (x >= width()) || (y < 0) || (y >= height())) return;
+  if ((x < 0) || (x >= width()) || (y < 0) || (y >= height()))
+    return;
 
   // check rotation, move pixel around if necessary
   switch (getRotation())
   {
-    case 1:
-      swap(x, y);
-      x = HINKE0154A35_WIDTH - x - 1;
-      break;
-    case 2:
-      x = HINKE0154A35_WIDTH - x - 1;
-      y = HINKE0154A35_HEIGHT - y - 1;
-      break;
-    case 3:
-      swap(x, y);
-      y = HINKE0154A35_HEIGHT - y - 1;
-      break;
+  case 1:
+    swap(x, y);
+    x = HINKE0154A35_WIDTH - x - 1;
+    break;
+  case 2:
+    x = HINKE0154A35_WIDTH - x - 1;
+    y = HINKE0154A35_HEIGHT - y - 1;
+    break;
+  case 3:
+    swap(x, y);
+    y = HINKE0154A35_HEIGHT - y - 1;
+    break;
   }
   uint16_t i = x / 8 + y * HINKE0154A35_WIDTH / 8;
   if (_current_page < 1)
   {
-    if (i >= sizeof(_black_buffer)) return;
+    if (i >= sizeof(_black_buffer))
+      return;
   }
   else
   {
     y -= _current_page * HINKE0154A35_PAGE_HEIGHT;
-    if ((y < 0) || (y >= HINKE0154A35_PAGE_HEIGHT)) return;
+    if ((y < 0) || (y >= HINKE0154A35_PAGE_HEIGHT))
+      return;
     i = x / 8 + y * HINKE0154A35_WIDTH / 8;
   }
 
   _black_buffer[i] = (_black_buffer[i] & (0xFF ^ (1 << (7 - x % 8)))); // white
 
   _red_buffer[i] = (_red_buffer[i] | (1 << (7 - x % 8))); // white
-  if (color == GxEPD_WHITE) return;
-  else if (color == GxEPD_BLACK) _black_buffer[i] = (_black_buffer[i] | (1 << (7 - x % 8)));
-  else if (color == GxEPD_RED)_red_buffer[i] = (_red_buffer[i] & (0xFF ^ (1 << (7 - x % 8))));
+  if (color == GxEPD_WHITE)
+    return;
+  else if (color == GxEPD_BLACK)
+    _black_buffer[i] = (_black_buffer[i] | (1 << (7 - x % 8)));
+  else if (color == GxEPD_RED)
+    _red_buffer[i] = (_red_buffer[i] & (0xFF ^ (1 << (7 - x % 8))));
   else
   {
-    if ((color & 0xF100) > (0xF100 / 2))_red_buffer[i] = (_red_buffer[i] & (0xFF ^ (1 << (7 - x % 8))));
+    if ((color & 0xF100) > (0xF100 / 2))
+      _red_buffer[i] = (_red_buffer[i] & (0xFF ^ (1 << (7 - x % 8))));
     else if ((((color & 0xF100) >> 11) + ((color & 0x07E0) >> 5) + (color & 0x001F)) < 3 * 255 / 2)
     {
       _black_buffer[i] = (_black_buffer[i] | (1 << (7 - x % 8)));
     }
   }
 }
-
 
 void HINKE0154A35::init(uint32_t serial_diag_bitrate)
 {
@@ -100,11 +106,16 @@ void HINKE0154A35::fillScreen(uint16_t color)
 {
   uint8_t black = 0x00;
   uint8_t red = 0xFF;
-  if (color == GxEPD_WHITE);
-  else if (color == GxEPD_BLACK) black = 0xFF;
-  else if (color == GxEPD_RED) red = 0x00;
-  else if ((color & 0xF100) > (0xF100 / 2))  red = 0x00;
-  else if ((((color & 0xF100) >> 11) + ((color & 0x07E0) >> 5) + (color & 0x001F)) < 3 * 255 / 2) black = 0xFF;
+  if (color == GxEPD_WHITE)
+    ;
+  else if (color == GxEPD_BLACK)
+    black = 0xFF;
+  else if (color == GxEPD_RED)
+    red = 0x00;
+  else if ((color & 0xF100) > (0xF100 / 2))
+    red = 0x00;
+  else if ((((color & 0xF100) >> 11) + ((color & 0x07E0) >> 5) + (color & 0x001F)) < 3 * 255 / 2)
+    black = 0xFF;
   for (uint16_t x = 0; x < sizeof(_black_buffer); x++)
   {
     _black_buffer[x] = black;
@@ -112,9 +123,32 @@ void HINKE0154A35::fillScreen(uint16_t color)
   }
 }
 
+void HINKE0154A35::drawBitmap(const uint8_t *bitmap, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color, int16_t mode)
+{
+  if (mode & bm_default)
+    mode |= bm_invert;
+  drawBitmapBM(bitmap, x, y, w, h, color, mode);
+}
+
+void HINKE0154A35::drawExamplePicture(const uint8_t *black_bitmap, const uint8_t *red_bitmap, uint32_t black_size, uint32_t red_size)
+{
+  drawPicture(black_bitmap, red_bitmap, black_size, red_size, bm_invert_red);
+}
+
+void HINKE0154A35::drawPicture(const uint8_t *black_bitmap, const uint8_t *red_bitmap, uint32_t black_size, uint32_t red_size, int16_t mode)
+{
+  drawBitmapBM(black_bitmap, 0, 0, HINKE0154A35_WIDTH, HINKE0154A35_HEIGHT, GxEPD_BLACK, mode);
+  drawBitmapBM(red_bitmap, 0, 0, HINKE0154A35_WIDTH, HINKE0154A35_HEIGHT, GxEPD_RED, mode);
+}
+
+void HINKE0154A35::drawBitmap(const uint8_t *bitmap, uint32_t size, int16_t mode)
+{
+  drawBitmapBM(bitmap, 0, 0, HINKE0154A35_WIDTH, HINKE0154A35_HEIGHT, GxEPD_BLACK, mode);
+}
+
 void HINKE0154A35::update(void)
 {
- // reset required for wakeup
+  // reset required for wakeup
   if (_rst >= 0)
   {
     digitalWrite(_rst, 0);
@@ -122,92 +156,98 @@ void HINKE0154A35::update(void)
     digitalWrite(_rst, 1);
     delay(10);
   }
-  //1.54 Zoll Display: 
+  // 1.54 Zoll Display:
   _writeCommand(0x12);
   _waitWhileBusy("SendResetfirst");
   delay(5);
 
   _writeCommand(0x74);
-  _writeData (0x54);
+  _writeData(0x54);
 
   _writeCommand(0x7E);
-  _writeData (0x3B);
+  _writeData(0x3B);
 
   _writeCommand(0x2B);
-  _writeData (0x04);
-  _writeData (0x63);
+  _writeData(0x04);
+  _writeData(0x63);
 
   _writeCommand(0x0C);
-  _writeData (0x8F);
-  _writeData (0x8F);
-  _writeData (0x8F);
-  _writeData (0x3F);
+  _writeData(0x8F);
+  _writeData(0x8F);
+  _writeData(0x8F);
+  _writeData(0x3F);
 
   _writeCommand(0x01);
-  _writeData (0x97);
-  _writeData (0x00);
-  _writeData (0x00);
+  _writeData(0x97);
+  _writeData(0x00);
+  _writeData(0x00);
 
   _writeCommand(0x11);
-  _writeData (0x02);
+  _writeData(0x02);
 
   _writeCommand(0x44);
-  _writeData (0x12);
-  _writeData (0x00);
+  _writeData(0x12);
+  _writeData(0x00);
 
   _writeCommand(0x45);
-  _writeData (0x00);
-  _writeData (0x00);
-  _writeData (0x97);
-  _writeData (0x00);
+  _writeData(0x00);
+  _writeData(0x00);
+  _writeData(0x97);
+  _writeData(0x00);
 
-  _writeCommand (0x3C);
-  _writeData (0x01);
-  _writeCommand (0x18);
-  _writeData (0x80);
-  _writeCommand (0x22);
-  _writeData (0xB1);
-  _writeCommand (0x20);
+  _writeCommand(0x3C);
+  _writeData(0x01);
+  _writeCommand(0x18);
+  _writeData(0x80);
+  _writeCommand(0x22);
+  _writeData(0xB1);
+  _writeCommand(0x20);
   _waitWhileBusy("SendResetmiddle");
   delay(5);
 
-  _writeCommand (0x1B);
-  _writeCommand (0x0C);
-  _writeCommand (0xFF);
+  _writeCommand(0x1B);
+  _writeCommand(0x0C);
+  _writeCommand(0xFF);
 
-  _writeCommand (0x4E);
-  _writeData (0x12);
+  _writeCommand(0x4E);
+  _writeData(0x12);
 
-  _writeCommand (0x4F);
-  _writeData (0x00);
-  _writeData (0x00);
+  _writeCommand(0x4F);
+  _writeData(0x00);
+  _writeData(0x00);
 
-  _writeCommand (0x26);
+  _writeCommand(0x26);
 
   for (uint32_t i = 0; i < HINKE0154A35_BUFFER_SIZE; i++)
   {
     _writeData((i < sizeof(_red_buffer)) ? ~_red_buffer[i] : 0xFF);
   }
 
-  _writeCommand (0x4E);
-  _writeData (0x12);
+  _writeCommand(0x4E);
+  _writeData(0x12);
 
-  _writeCommand (0x4F);
-  _writeData (0x00);
-  _writeData (0x00);
+  _writeCommand(0x4F);
+  _writeData(0x00);
+  _writeData(0x00);
 
-  _writeCommand (0x24);
+  _writeCommand(0x24);
 
   for (uint32_t i = 0; i < HINKE0154A35_BUFFER_SIZE; i++)
   {
     _writeData((i < sizeof(_black_buffer)) ? ~_black_buffer[i] : 0xFF);
   }
 
-  _writeCommand (0x22);
-  _writeData (0xC7);
+  _writeCommand(0x22);
+  _writeData(0xC7);
 
-  _writeCommand (0x20);
-  _waitWhileBusy("SEND DONE");  
+  _writeCommand(0x20);
+  _waitWhileBusy("SEND DONE");
+}
+
+void HINKE0154A35::powerDown()
+{
+  _using_partial_mode = false; // force _wakeUp()
+  _sleep();
 }
 
 void HINKE0154A35::_writeCommand(uint8_t command)
@@ -220,16 +260,18 @@ void HINKE0154A35::_writeData(uint8_t data)
   IO.writeDataTransaction(data);
 }
 
-void HINKE0154A35::_waitWhileBusy(const char* comment)
+void HINKE0154A35::_waitWhileBusy(const char *comment)
 {
   unsigned long start = micros();
   while (1)
   {
-    if (digitalRead(_busy) == 0) break;
+    if (digitalRead(_busy) == 0)
+      break;
     delay(1);
     if (micros() - start > 30000000) // >14.9s !
     {
-      if (_diag_enabled) Serial.println("Busy Timeout!");
+      if (_diag_enabled)
+        Serial.println("Busy Timeout!");
       break;
     }
   }
@@ -245,26 +287,97 @@ void HINKE0154A35::_waitWhileBusy(const char* comment)
     }
 #endif
   }
-  (void) start;
+  (void)start;
 }
 
-void HINKE0154A35::_rotate(uint16_t& x, uint16_t& y, uint16_t& w, uint16_t& h)
+void HINKE0154A35::_rotate(uint16_t &x, uint16_t &y, uint16_t &w, uint16_t &h)
 {
   switch (getRotation())
   {
-    case 1:
-      swap(x, y);
-      swap(w, h);
-      x = HINKE0154A35_WIDTH - x - w - 1;
-      break;
-    case 2:
-      x = HINKE0154A35_WIDTH - x - w - 1;
-      y = HINKE0154A35_HEIGHT - y - h - 1;
-      break;
-    case 3:
-      swap(x, y);
-      swap(w, h);
-      y = HINKE0154A35_HEIGHT - y - h - 1;
-      break;
+  case 1:
+    swap(x, y);
+    swap(w, h);
+    x = HINKE0154A35_WIDTH - x - w - 1;
+    break;
+  case 2:
+    x = HINKE0154A35_WIDTH - x - w - 1;
+    y = HINKE0154A35_HEIGHT - y - h - 1;
+    break;
+  case 3:
+    swap(x, y);
+    swap(w, h);
+    y = HINKE0154A35_HEIGHT - y - h - 1;
+    break;
   }
+}
+
+void HINKE0154A35::_wakeUp()
+{
+  // reset required for wakeup
+  if (_rst >= 0)
+  {
+    digitalWrite(_rst, 0);
+    delay(10);
+    digitalWrite(_rst, 1);
+    delay(10);
+  }
+  /*
+    _writeCommand(0x06);
+    _writeData (0x17);
+    _writeData (0x17);
+    _writeData (0x17);
+    _writeCommand(0x04);
+    _waitWhileBusy("_wakeUp Power On");
+    _writeCommand(0X00);
+    _writeData(0x8f);
+    _writeCommand(0X50);
+    _writeData(0x77);
+    _writeCommand(0x61);
+    _writeData (0x80);
+    _writeData (0x01);
+    _writeData (0x28);
+    */
+}
+
+void HINKE0154A35::_sleep(void)
+{
+  _waitWhileBusy("_sleep Going to sleep.");
+  if (_rst >= 0)
+  {
+    _writeCommand(0x10); // sleep!
+    _writeData(0x11);
+  }
+}
+
+void HINKE0154A35::drawCornerTest(uint8_t em)
+{
+  if (_current_page != -1)
+    return;
+  _using_partial_mode = false;
+  _wakeUp();
+  _writeCommand(0x10);
+  for (uint32_t y = 0; y < HINKE0154A35_HEIGHT; y++)
+  {
+    for (uint32_t x = 0; x < HINKE0154A35_WIDTH / 8; x++)
+    {
+      uint8_t data = 0xFF; // white is 0xFF on device
+      if ((x < 1) && (y < 8))
+        data = 0x00;
+      if ((x > HINKE0154A35_WIDTH / 8 - 3) && (y < 16))
+        data = 0x00;
+      if ((x > HINKE0154A35_WIDTH / 8 - 4) && (y > HINKE0154A35_HEIGHT - 25))
+        data = 0x00;
+      if ((x < 4) && (y > HINKE0154A35_HEIGHT - 33))
+        data = 0x00;
+      _writeData(data);
+    }
+  }
+  _writeCommand(0x13);
+  for (uint32_t i = 0; i < HINKE0154A35_BUFFER_SIZE; i++)
+  {
+    _writeData(0xFF); // white is 0xFF on device
+  }
+  _writeCommand(0x12); // display refresh
+  _waitWhileBusy("drawCornerTest");
+  _sleep();
 }
--- src/HINKE042A11/HINKE042A11.h
+++ src/HINKE042A11/HINKE042A11.h
@@ -2,7 +2,7 @@
 #define _HINKE042A11_H_
 
 #include "../GxEPD.h"
-//4.2"
+// 4.2"
 #define HINKE042A11_WIDTH 400
 #define HINKE042A11_HEIGHT 300
 
@@ -16,50 +16,60 @@
 
 class HINKE042A11 : public GxEPD
 {
-  public:
+public:
 #if defined(ESP8266)
-    //HINKE042A11(GxIO& io, int8_t rst = D4, int8_t busy = D2);
-    // use pin numbers, other ESP8266 than Wemos may not use Dx names
-    HINKE042A11(GxIO& io, int8_t rst = 2, int8_t busy = 4);
+  // HINKE042A11(GxIO& io, int8_t rst = D4, int8_t busy = D2);
+  //  use pin numbers, other ESP8266 than Wemos may not use Dx names
+  HINKE042A11(GxIO &io, int8_t rst = 2, int8_t busy = 4);
 #else
-    HINKE042A11(GxIO& io, int8_t rst = 9, int8_t busy = 7);
+  HINKE042A11(GxIO &io, int8_t rst = 9, int8_t busy = 7);
 #endif
-    void drawPixel(int16_t x, int16_t y, uint16_t color);
-    void init(uint32_t serial_diag_bitrate = 0); // = 0 : disabled
-    void fillScreen(uint16_t color); // to buffer
-    void update(void);
-    // to buffer, may be cropped, drawPixel() used, update needed
-    void drawExamplePicture(const uint8_t* black_bitmap, const uint8_t* red_bitmap, uint32_t black_size, uint32_t red_size);
-    // to full screen, filled with white if size is less, no update needed, black  /white / red, general version
-    void drawPicture(const uint8_t* black_bitmap, const uint8_t* red_bitmap, uint32_t black_size, uint32_t red_size, int16_t mode = bm_normal);
-    // to full screen, filled with white if size is less, no update needed
-     void powerDown();
-  private:
-    template <typename T> static inline void
-    swap(T& a, T& b)
-    {
-      T t = a;
-      a = b;
-      b = t;
-    }
-    void _writeData(uint8_t data);
-    void _writeCommand(uint8_t command);
-    void _waitWhileBusy(const char* comment = 0);
-    void _rotate(uint16_t& x, uint16_t& y, uint16_t& w, uint16_t& h);
-  private:
+  void drawPixel(int16_t x, int16_t y, uint16_t color);
+  void init(uint32_t serial_diag_bitrate = 0); // = 0 : disabled
+  void fillScreen(uint16_t color);             // to buffer
+  void update(void);
+  // to buffer, may be cropped, drawPixel() used, update needed
+  void drawBitmap(const uint8_t *bitmap, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color, int16_t mode = bm_normal);
+  // to buffer, may be cropped, drawPixel() used, update needed
+  void drawExamplePicture(const uint8_t *black_bitmap, const uint8_t *red_bitmap, uint32_t black_size, uint32_t red_size);
+  // to full screen, filled with white if size is less, no update needed, black  /white / red, general version
+  void drawPicture(const uint8_t *black_bitmap, const uint8_t *red_bitmap, uint32_t black_size, uint32_t red_size, int16_t mode = bm_normal);
+  // to full screen, filled with white if size is less, no update needed
+  void drawBitmap(const uint8_t *bitmap, uint32_t size, int16_t mode = bm_normal); // only bm_normal, bm_invert, bm_partial_update modes implemented
+  // to full screen, filled with white if size is less, no update needed
+  void powerDown();
+  void drawCornerTest(uint8_t em = 0x01);
+
+private:
+  template <typename T>
+  static inline void
+  swap(T &a, T &b)
+  {
+    T t = a;
+    a = b;
+    b = t;
+  }
+  void _writeData(uint8_t data);
+  void _writeCommand(uint8_t command);
+  void _wakeUp();
+  void _sleep();
+  void _waitWhileBusy(const char *comment = 0);
+  void _rotate(uint16_t &x, uint16_t &y, uint16_t &w, uint16_t &h);
+
+private:
 #if defined(__AVR)
-    uint8_t _black_buffer[HINKE042A11_PAGE_SIZE];
-    uint8_t _red_buffer[HINKE042A11_PAGE_SIZE];
+  uint8_t _black_buffer[HINKE042A11_PAGE_SIZE];
+  uint8_t _red_buffer[HINKE042A11_PAGE_SIZE];
 #else
-    uint8_t _black_buffer[HINKE042A11_BUFFER_SIZE];
-    uint8_t _red_buffer[HINKE042A11_BUFFER_SIZE];
+  uint8_t _black_buffer[HINKE042A11_BUFFER_SIZE];
+  uint8_t _red_buffer[HINKE042A11_BUFFER_SIZE];
 #endif
-    GxIO& IO;
-    int16_t _current_page;
-    bool _using_partial_mode;
-    bool _diag_enabled;
-    int8_t _rst;
-    int8_t _busy;
+  GxIO &IO;
+  int16_t _current_page;
+  bool _using_partial_mode;
+  bool _diag_enabled;
+  int8_t _rst;
+  int8_t _busy;
 };
 
 #ifndef GxEPD_Class
