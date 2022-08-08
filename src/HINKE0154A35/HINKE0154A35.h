51596c3f311333807c2025a9c86e2a0bb88dbe04
foorschtbar <mail@wischmopp90.com>
2022-08-08 20:29:21 +0200

    Fixed HINKE0154A35 and HINKE042A11

--- src/HINKE0154A35/HINKE0154A35.h
+++ src/HINKE0154A35/HINKE0154A35.h
@@ -2,7 +2,7 @@
 #define _HINKE0154A35_H_
 
 #include "../GxEPD.h"
-//1.54"
+// 1.54"
 #define HINKE0154A35_WIDTH 152
 #define HINKE0154A35_HEIGHT 152
 
@@ -16,50 +16,60 @@
 
 class HINKE0154A35 : public GxEPD
 {
-  public:
+public:
 #if defined(ESP8266)
-    //HINKE0154A35(GxIO& io, int8_t rst = D4, int8_t busy = D2);
-    // use pin numbers, other ESP8266 than Wemos may not use Dx names
-    HINKE0154A35(GxIO& io, int8_t rst = 2, int8_t busy = 4);
+  // HINKE0154A35(GxIO& io, int8_t rst = D4, int8_t busy = D2);
+  //  use pin numbers, other ESP8266 than Wemos may not use Dx names
+  HINKE0154A35(GxIO &io, int8_t rst = 2, int8_t busy = 4);
 #else
-    HINKE0154A35(GxIO& io, int8_t rst = 9, int8_t busy = 7);
+  HINKE0154A35(GxIO &io, int8_t rst = 9, int8_t busy = 7);
 #endif
-    void drawPixel(int16_t x, int16_t y, uint16_t color);
-    void init(uint32_t serial_diag_bitrate = 0); // = 0 : disabled
-    void fillScreen(uint16_t color); // to buffer
-    void update(void);
-    // to buffer, may be cropped, drawPixel() used, update needed
-    void drawExamplePicture(const uint8_t* black_bitmap, const uint8_t* red_bitmap, uint32_t black_size, uint32_t red_size);
-    // to full screen, filled with white if size is less, no update needed, black  /white / red, general version
-    void drawPicture(const uint8_t* black_bitmap, const uint8_t* red_bitmap, uint32_t black_size, uint32_t red_size, int16_t mode = bm_normal);
-    // to full screen, filled with white if size is less, no update needed
-     void powerDown();
-  private:
-    template <typename T> static inline void
-    swap(T& a, T& b)
-    {
-      T t = a;
-      a = b;
-      b = t;
-    }
-    void _writeData(uint8_t data);
-    void _writeCommand(uint8_t command);
-    void _waitWhileBusy(const char* comment = 0);
-    void _rotate(uint16_t& x, uint16_t& y, uint16_t& w, uint16_t& h);
-  private:
+  void drawPixel(int16_t x, int16_t y, uint16_t color);
+  void init(uint32_t serial_diag_bitrate = 0); // = 0 : disabled
+  void fillScreen(uint16_t color);             // to buffer
+  void update(void);
+  // to buffer, may be cropped, drawPixel() used, update needed
+  void drawBitmap(const uint8_t *bitmap, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color, int16_t mode = bm_normal);
+  // to buffer, may be cropped, drawPixel() used, update needed
+  void drawExamplePicture(const uint8_t *black_bitmap, const uint8_t *red_bitmap, uint32_t black_size, uint32_t red_size);
+  // to full screen, filled with white if size is less, no update needed, black  /white / red, general version
+  void drawPicture(const uint8_t *black_bitmap, const uint8_t *red_bitmap, uint32_t black_size, uint32_t red_size, int16_t mode = bm_normal);
+  // to full screen, filled with white if size is less, no update needed
+  void drawBitmap(const uint8_t *bitmap, uint32_t size, int16_t mode = bm_normal); // only bm_normal, bm_invert, bm_partial_update modes implemented
+  // to full screen, filled with white if size is less, no update needed
+  void powerDown();
+  void drawCornerTest(uint8_t em = 0x01);
+
+private:
+  template <typename T>
+  static inline void
+  swap(T &a, T &b)
+  {
+    T t = a;
+    a = b;
+    b = t;
+  }
+  void _writeData(uint8_t data);
+  void _writeCommand(uint8_t command);
+  void _wakeUp();
+  void _sleep();
+  void _waitWhileBusy(const char *comment = 0);
+  void _rotate(uint16_t &x, uint16_t &y, uint16_t &w, uint16_t &h);
+
+private:
 #if defined(__AVR)
-    uint8_t _black_buffer[HINKE0154A35_PAGE_SIZE];
-    uint8_t _red_buffer[HINKE0154A35_PAGE_SIZE];
+  uint8_t _black_buffer[HINKE0154A35_PAGE_SIZE];
+  uint8_t _red_buffer[HINKE0154A35_PAGE_SIZE];
 #else
-    uint8_t _black_buffer[HINKE0154A35_BUFFER_SIZE];
-    uint8_t _red_buffer[HINKE0154A35_BUFFER_SIZE];
+  uint8_t _black_buffer[HINKE0154A35_BUFFER_SIZE];
+  uint8_t _red_buffer[HINKE0154A35_BUFFER_SIZE];
 #endif
-    GxIO& IO;
-    int16_t _current_page;
-    bool _using_partial_mode;
-    bool _diag_enabled;
-    int8_t _rst;
-    int8_t _busy;
+  GxIO &IO;
+  int16_t _current_page;
+  bool _using_partial_mode;
+  bool _diag_enabled;
+  int8_t _rst;
+  int8_t _busy;
 };
 
 #ifndef GxEPD_Class
